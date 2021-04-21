c4ae3a967ba85f169adfb0ab3694be971ee4eeff
Jeroen <jeroensmink98@gmail.com>
2021-04-21 11:00:13 +0200

    Initial groundwork for production connector

--- sse-aai-zsa/Zsa/ExtentionService_zsa.py
+++ sse-aai-zsa/Zsa/ExtentionService_zsa.py
@@ -6,12 +6,26 @@ import logging.config
 import os
 import sys
 import time
+import subprocess
 from concurrent import futures
 from datetime import datetime
 
+
+# Import config file
+from configparser import ConfigParser
+
+config = ConfigParser()
+
+config.read("config.ini")
+
+
+# Set the filepath of the batscript
+filepath = config.get("main", "bat_path") + "/run_all_scripts.bat"
+
+
 # Add Generated folder to module path.
 PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.join(PARENT_DIR, 'Generated'))
+sys.path.append(os.path.join(PARENT_DIR, "Generated"))
 
 import ServerSideExtension_pb2 as SSE
 import grpc
@@ -33,11 +47,13 @@ class ExtensionService(SSE.ConnectorServicer):
         """
         self._function_definitions = funcdef_file
         self.ScriptEval = ScriptEval()
-        os.makedirs('logs', exist_ok=True)
-        log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logger.config')
+        os.makedirs("logs", exist_ok=True)
+        log_file = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "logger.config"
+        )
         logging.config.fileConfig(log_file)
-        logging.info('Starting Zehnder Source Automation Connector')
-        logging.info('Logging enabled')
+        logging.info("Starting Zehnder Source Automation Connector")
+        logging.info("Logging enabled")
 
     @property
     def function_definitions(self):
@@ -52,11 +68,7 @@ class ExtensionService(SSE.ConnectorServicer):
         :return: Mapping of function id and implementation
         """
         return {
-            0: '_zsa',
-            1: '_zsa_aggr',
-            2: '_cache',
-            3: '_no_cache',
-            4: '_echo_table'
+            0: "_run_scripts",
         }
 
     @staticmethod
@@ -68,7 +80,7 @@ class ExtensionService(SSE.ConnectorServicer):
         """
         metadata = dict(context.invocation_metadata())
         header = SSE.FunctionRequestHeader()
-        header.ParseFromString(metadata['qlik-functionrequestheader-bin'])
+        header.ParseFromString(metadata["qlik-functionrequestheader-bin"])
 
         return header.functionId
 
@@ -77,107 +89,20 @@ class ExtensionService(SSE.ConnectorServicer):
     """
 
     @staticmethod
-    def _zsa(request, context):
+    def _run_scripts(request, context):
         """
-        Mirrors the input and sends back the same data.
+        Call for the bat script to run all python scripts in the scripts folder
         :param request: iterable sequence of bundled rows
         :return: the same iterable sequence as received
         """
-        for request_rows in request:
-            yield request_rows
 
-    @staticmethod
-    def _zsa_aggr(request, context):
-        """
-        Aggregates the parameters to a single comma separated string.
-        :param request: iterable sequence of bundled rows
-        :return: string
-        """
-        params = []
+        # Create a subprocess based on the file in the filepath
+        run_all_scripts = subprocess.Popen(filepath)
 
-        # Iterate over bundled rows
-        for request_rows in request:
-            # Iterate over rows
-            for row in request_rows.rows:
-                # Retrieve string value of parameter and append to the params variable
-                # Length of param is 1 since one column is received, the [0] collects the first value in the list
-                param = [d.strData for d in row.duals][0]
-                params.append(param)
+        stdout, stderr = run_all_scripts.communicate()
 
-        # Aggregate parameters to a single string
-        result = ', '.join(params)
-
-        # Create an iterable of dual with the result
-        duals = iter([SSE.Dual(strData=result)])
-
-        # Yield the row data as bundled rows
-        yield SSE.BundledRows(rows=[SSE.Row(duals=duals)])
-
-    @staticmethod
-    def _cache(request, context):
-        """
-        Cache enabled. Add the datetime stamp to the end of each string value.
-        :param request: iterable sequence of bundled rows
-        :param context: not used.
-        :return: string
-        """
-        # Iterate over bundled rows
-        for request_rows in request:
-            # Iterate over rows
-            for row in request_rows.rows:
-                # Retrieve string value of parameter and append to the params variable
-                # Length of param is 1 since one column is received, the [0] collects the first value in the list
-                param = [d.strData for d in row.duals][0]
-
-                # Join with current timedate stamp
-                result = param + ' ' + datetime.now().isoformat()
-                # Create an iterable of dual with the result
-                duals = iter([SSE.Dual(strData=result)])
-
-                # Yield the row data as bundled rows
-                yield SSE.BundledRows(rows=[SSE.Row(duals=duals)])
-
-    @staticmethod
-    def _no_cache(request, context):
-        """
-        Cache disabled. Add the datetime stamp to the end of each string value.
-        :param request:
-        :param context: used for disabling the cache in the header.
-        :return: string
-        """
-        # Disable caching.
-        md = (('qlik-cache', 'no-store'),)
-        context.send_initial_metadata(md)
-
-        # Iterate over bundled rows
-        for request_rows in request:
-            # Iterate over rows
-            for row in request_rows.rows:
-                # Retrieve string value of parameter and append to the params variable
-                # Length of param is 1 since one column is received, the [0] collects the first value in the list
-                param = [d.strData for d in row.duals][0]
-
-                # Join with current timedate stamp
-                result = param + ' ' + datetime.now().isoformat()
-                # Create an iterable of dual with the result
-                duals = iter([SSE.Dual(strData=result)])
-
-                # Yield the row data as bundled rows
-                yield SSE.BundledRows(rows=[SSE.Row(duals=duals)])
-
-    @staticmethod
-    def _echo_table(request, context):
-        """
-        Echo the input table.
-        :param request:
-        :param context:
-        :return:
-        """
-        for request_rows in request:
-            response_rows = []
-            for row in request_rows.rows:
-                response_rows.append(row)
-            yield SSE.BundledRows(rows=response_rows)
+        # Return status code (if 0 its good)
+        yield run_all_scripts.returncode
 
     """
     Implementation of rpc functions.
@@ -193,30 +118,35 @@ class ExtensionService(SSE.ConnectorServicer):
         :param context: the context, not used in this method.
         :return: the capabilities.
         """
-        logging.info('GetCapabilities')
+        logging.info("GetCapabilities")
         # Create an instance of the Capabilities grpc message
         # Enable(or disable) script evaluation
         # Set values for pluginIdentifier and pluginVersion
-        capabilities = SSE.Capabilities(allowScript=True,
-                                        pluginIdentifier='Hello World - Qlik',
-                                        pluginVersion='v1.1.0')
+        capabilities = SSE.Capabilities(
+            allowScript=True,
+            pluginIdentifier="ZSAs - Qlik",
+            pluginVersion="v1.1.0",
+        )
 
         # If user defined functions supported, add the definitions to the message
         with open(self.function_definitions) as json_file:
             # Iterate over each function definition and add data to the capabilities grpc message
-            for definition in json.load(json_file)['Functions']:
+            for definition in json.load(json_file)["Functions"]:
                 function = capabilities.functions.add()
-                function.name = definition['Name']
-                function.functionId = definition['Id']
-                function.functionType = definition['Type']
-                function.returnType = definition['ReturnType']
+                function.name = definition["Name"]
+                function.functionId = definition["Id"]
+                function.functionType = definition["Type"]
+                function.returnType = definition["ReturnType"]
 
                 # Retrieve name and type of each parameter
-                for param_name, param_type in sorted(definition['Params'].items()):
+                for param_name, param_type in sorted(definition["Params"].items()):
                     function.params.add(name=param_name, dataType=param_type)
 
-                logging.info('Adding to capabilities: {}({})'.format(function.name,
-                                                                     [p.name for p in function.params]))
+                logging.info(
+                    "Adding to capabilities: {}({})".format(
+                        function.name, [p.name for p in function.params]
+                    )
+                )
 
         return capabilities
 
@@ -231,7 +161,7 @@ class ExtensionService(SSE.ConnectorServicer):
         func_id = self._get_function_id(context)
 
         # Call corresponding function
-        logging.info('ExecuteFunction (functionId: {})'.format(func_id))
+        logging.info("ExecuteFunction (functionId: {})".format(func_id))
 
         return getattr(self, self.functions[func_id])(request_iterator, context)
 
@@ -245,18 +175,22 @@ class ExtensionService(SSE.ConnectorServicer):
         # Parse header for script request
         metadata = dict(context.invocation_metadata())
         header = SSE.ScriptRequestHeader()
-        header.ParseFromString(metadata['qlik-scriptrequestheader-bin'])
+        header.ParseFromString(metadata["qlik-scriptrequestheader-bin"])
 
         # Retrieve function type
         func_type = self.ScriptEval.get_func_type(header)
 
         # Verify function type
-        if (func_type == FunctionType.Aggregation) or (func_type == FunctionType.Tensor):
+        if (func_type == FunctionType.Aggregation) or (
+            func_type == FunctionType.Tensor
+        ):
             return self.ScriptEval.EvaluateScript(header, request, context, func_type)
         else:
             # This plugin does not support other function types than aggregation  and tensor.
             # Make sure the error handling, including logging, works as intended in the client
-            msg = 'Function type {} is not supported in this plugin.'.format(func_type.name)
+            msg = "Function type {} is not supported in this plugin.".format(
+                func_type.name
+            )
             context.set_code(grpc.StatusCode.UNIMPLEMENTED)
             context.set_details(msg)
             # Raise error on the plugin-side
@@ -279,19 +213,25 @@ class ExtensionService(SSE.ConnectorServicer):
 
         if pem_dir:
             # Secure connection
-            with open(os.path.join(pem_dir, 'sse_server_key.pem'), 'rb') as f:
+            with open(os.path.join(pem_dir, "sse_server_key.pem"), "rb") as f:
                 private_key = f.read()
-            with open(os.path.join(pem_dir, 'sse_server_cert.pem'), 'rb') as f:
+            with open(os.path.join(pem_dir, "sse_server_cert.pem"), "rb") as f:
                 cert_chain = f.read()
-            with open(os.path.join(pem_dir, 'root_cert.pem'), 'rb') as f:
+            with open(os.path.join(pem_dir, "root_cert.pem"), "rb") as f:
                 root_cert = f.read()
-            credentials = grpc.ssl_server_credentials([(private_key, cert_chain)], root_cert, True)
-            server.add_secure_port('[::]:{}'.format(port), credentials)
-            logging.info('*** Running server in secure mode on port: {} ***'.format(port))
+            credentials = grpc.ssl_server_credentials(
+                [(private_key, cert_chain)], root_cert, True
+            )
+            server.add_secure_port("[::]:{}".format(port), credentials)
+            logging.info(
+                "*** Running server in secure mode on port: {} ***".format(port)
+            )
         else:
             # Insecure connection
-            server.add_insecure_port('[::]:{}'.format(port))
-            logging.info('*** Running server in insecure mode on port: {} ***'.format(port))
+            server.add_insecure_port("[::]:{}".format(port))
+            logging.info(
+                "*** Running server in insecure mode on port: {} ***".format(port)
+            )
 
         # Start gRPC server
         server.start()
@@ -302,15 +242,17 @@ class ExtensionService(SSE.ConnectorServicer):
             server.stop(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', nargs='?', default='50052')
-    parser.add_argument('--pem_dir', nargs='?')
-    parser.add_argument('--definition_file', nargs='?', default='FuncDefs_zsa.json')
+    parser.add_argument("--port", nargs="?", default="50052")
+    parser.add_argument("--pem_dir", nargs="?")
+    parser.add_argument("--definition_file", nargs="?", default="FuncDefs_zsa.json")
     args = parser.parse_args()
 
     # need to locate the file when script is called from outside it's location dir.
-    def_file = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), args.definition_file)
+    def_file = os.path.join(
+        os.path.dirname(os.path.realpath(sys.argv[0])), args.definition_file
+    )
 
     calc = ExtensionService(def_file)
     calc.Serve(args.port, args.pem_dir)
---
+++ sse-aai-zsa/Zsa/bat/run_all_scripts.bat
@@ -0,0 +1,5 @@
+@echo off
+
+cd ./scripts
+
+for %%A IN (*.py) do start /b /wait "" python "%%~fA"
